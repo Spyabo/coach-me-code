6de669f96f914eb18cb8b6346391e3978f3c4900
Spyabo <reesewright02@protonmail.com>
2023-05-02 11:42:02 +0100

    Add sign-in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,11 +1,12 @@
 import { SignIn } from "@clerk/nextjs/app-beta";
 
-export default function Page() {
+export default function Page({ searchParams }: any) {
+  const { redirectUrl } = searchParams;
   return (
     <section className="py-24 flex justify-center bg-neutral-50 h-full">
       <div className="container">
         <div className="flex justify-center">
-          <SignIn signUpUrl="/sign-up" />
+          <SignIn signUpUrl="/sign-up" redirectUrl={redirectUrl || "/"} />
         </div>
       </div>
     </section>
