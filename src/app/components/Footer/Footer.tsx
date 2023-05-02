75d906fe0547058751770d91ecaab3c82c619fd7
Spyabo <reesewright02@protonmail.com>
2023-05-02 16:13:28 +0100

    Add footer

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -1,5 +1,33 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="z-10 py-8 x-10 px-10 text-stone-400 bg-gray-800">
+      <div className="container">
+        <h5 className="text-lg">Coach Me Code</h5>
+        <p className="mt-4 text-sm text-stone-500">
+          &copy; {new Date().getFullYear()} Riff Raffs.
+        </p>
+        <div className="text-sm text-stone-400">
+          Developed by{" "}
+          <a
+            className="text-sky-600"
+            href="https://github.com/Spyabo/coach-me-code/graphs/contributors"
+            rel="noreferrer"
+            target="_blank"
+          >
+            Contributors
+          </a>{" "}
+          using{" "}
+          <a
+            className="text-sky-600"
+            href="https://beta.nextjs.org/docs/getting-started"
+            rel="noreferrer"
+            target="_blank"
+          >
+            NextJS 13 & Friends
+          </a>
+          .
+        </div>
+      </div>
+    </footer>
+  );
 }
