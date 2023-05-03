9fa3a5e18a62e72cecc3faffce9f11a2983a340b
Spyabo <reesewright02@protonmail.com>
2023-05-03 14:12:58 +0100

    Add listing form page

--- src/app/listings/create-listing/page.tsx
+++ src/app/listings/create-listing/page.tsx
@@ -2,7 +2,7 @@ import ListingForm from "@/app/components/ListingForm";
 
 export default async function Page() {
   return (
-    <div className="mx-auto flex px-10 py-10 bg-slate-300">
+    <div className="mx-auto flex justify-center px-10 py-10 bg-slate-300">
       <ListingForm />
     </div>
   );
