b608075fe3731dc1a2ed43e4c736ecc5e7d8bef5
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-03 17:10:33 +0100

    get user's first name in welcome message with clerk

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { useAuth, useUser } from "@clerk/nextjs";
+
+
 export default function Welcome() {
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    // In case the user signs out while on the page.
+    if (!isLoaded || !isSignedIn) {
+        return null;
+    }
     return (
         <div>
-            <h1>Welcome</h1>
+            <h1>Welcome, {user.firstName}</h1>
             <p>You took the first step! Whether you would like to sharpen the tools in your coding belt through mentoring others, or find a mentor to support you, this is the place! </p>
             <button><a href="/listings"> Explore Services</a></button>
             <button><a href="/listings/create-listing">Create a listing</a> </button>
