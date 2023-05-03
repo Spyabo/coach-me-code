6f15e67ae4bf18b16bf19642e48aa10bac3442a1
Spyabo <reesewright02@protonmail.com>
2023-05-03 15:32:39 +0100

    Fix import

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -6,17 +6,16 @@ import React, { useState } from "react";
 
 import { SignInButton, UserButton, useUser } from "@clerk/nextjs";
 import { SignedIn, SignedOut } from "@clerk/nextjs/app-beta/client";
-import { useUser } from "@clerk/nextjs";
 
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false);
   const { user } = useUser();
-  const {isSignedIn} = useUser();
-  const [walletDirection, setWalletDirection] = useState('');
+  const { isSignedIn } = useUser();
+  const [walletDirection, setWalletDirection] = useState("");
 
   const handleMoneyClick = () => {
-    if (isSignedIn) setWalletDirection('/wallet');
-    else if (!isSignedIn) setWalletDirection('/sign-in');
+    if (isSignedIn) setWalletDirection("/wallet");
+    else if (!isSignedIn) setWalletDirection("/sign-in");
   };
 
   return (
@@ -37,16 +36,13 @@ export default function Nav() {
                 </a>
               </div>
               <div className="flex flex-wrap ">
-                  <h1 className="text-white text-3xl font-bold md:pt-0 pl-6 justify-center hidden sm:block">
-                    Coach Me Code
-                  </h1>
+                <h1 className="text-white text-3xl font-bold md:pt-0 pl-6 justify-center hidden sm:block">
+                  Coach Me Code
+                </h1>
               </div>
 
-
               <div className="hidden md:block">
-
                 <div className="ml-10 flex items-baseline space-x-4">
-
                   <a
                     href="/listings"
                     className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
@@ -61,7 +57,6 @@ export default function Nav() {
                   >
                     Money
                   </a>
-
                 </div>
               </div>
             </div>
@@ -79,82 +74,79 @@ export default function Nav() {
                   </button>
                 </SignInButton>
               </SignedOut>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
+              <div className="-mr-2 flex md:hidden">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {!isOpen ? (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
           </div>
         </div>
 
         <Transition
-  show={isOpen}
-  enter="transition ease-out duration-100 transform"
-  enterFrom="opacity-0 scale-95"
-  enterTo="opacity-100 scale-100"
-  leave="transition ease-in duration-75 transform"
-  leaveFrom="opacity-100 scale-100"
-  leaveTo="opacity-0 scale-95"
->
-  {(ref) => (
-    <div className="md:hidden" id="mobile-menu">
-      <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-      <h1 className="text-white text-2xl font-bold pt-4 md:pt-0 md:text-3xl">
-                    Coach Me Code
-                  </h1>
-        <a
-          href="#"
-          className="hover:bg-yellow-100 hover:text-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
         >
-          Listings
-        </a>
-        
-
-      </div>
-    </div>
-  )}
-</Transition>
-
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <h1 className="text-white text-2xl font-bold pt-4 md:pt-0 md:text-3xl">
+                  Coach Me Code
+                </h1>
+                <a
+                  href="#"
+                  className="hover:bg-yellow-100 hover:text-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Listings
+                </a>
+              </div>
+            </div>
+          )}
+        </Transition>
       </nav>
     </div>
   );
