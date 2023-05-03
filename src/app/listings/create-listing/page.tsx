21526f7a2aacbb87d1b106322e8a79e9412f2ad3
staceyanne <staceywhite10@gmail.com>
2023-05-03 11:27:44 +0100

    updated form onsubmit functionality

--- src/app/listings/create-listing/page.tsx
+++ src/app/listings/create-listing/page.tsx
@@ -1,34 +1,78 @@
-
+"use client";
+import { useState } from "react";
 export default async function createListing() {
+  const [listingTitle, setListingTitle] = useState("");
+  const [listingDescription, setListingDescription] = useState("");
+  const [programmingLangauges, setProgrammingLanguages] = useState("");
+  const [listingTokens, setListingTokens] = useState(0);
 
-    return (
-    <form>
-  <div className="space-y-12">
-    <div className="border-b border-gray-900/10 pb-12">
-      <h1 className="text-base font-semibold leading-7 text-gray-900">Create a new listing</h1>
-      <p className="mt-1 text-sm leading-6 text-gray-600">This will allow you to create a new listing</p>
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log(listingTitle);
 
-      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div className="sm:col-span-4">
-          <label htmlFor="listingTitle" className="block text-sm font-medium leading-6 text-gray-900">Listing Title</label>
-          <div className="mt-2">
-            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-              <input type="text" name="listingTitle" id="listingTitle" autoComplete="listingTitle" className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" placeholder="your listing title...."/>
+    // const res = await fetch('/api/listings', {
+    //   method: 'POST',
+    //   body: formData,
+    // });
+    // const data = await res.json();
+    // console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h1 className="text-base font-semibold leading-7 text-gray-900">
+            Create a new listing
+          </h1>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            This will allow you to create a new listing
+          </p>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="listingTitle"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Listing Title
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                  <input
+                    type="text"
+                    name="listingTitle"
+                    id="listingTitle"
+                    autoComplete="listingTitle"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    value={listingTitle}
+                    onChange={(event) => setListingTitle(event.target.value)}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
 
-        <div className="col-span-full">
-          <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">Description</label>
-          <div className="mt-2">
-            <textarea id="description" name="description" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
-          </div>
-          <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about the service you would like to provide.</p>
-        </div>
+            <div className="col-span-full">
+              <label
+                htmlFor="description"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Description
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="description"
+                  name="description"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                ></textarea>
+              </div>
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                Write a few sentences about the service you would like to
+                provide.
+              </p>
+            </div>
 
-       
-        <div className="col-span-full">
+            {/* <div className="col-span-full">
           <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
           <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
             <div className="text-center">
@@ -45,36 +89,72 @@ export default async function createListing() {
               <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-
-    <div className="border-b border-gray-900/10 pb-12">
-      
-
-      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-        <div className="sm:col-span-3">
-          <label htmlFor="programming-languages" className="block text-sm font-medium leading-6 text-gray-900">Programming Languages</label>
-          <p className="mt-3 text-sm leading-6 text-gray-600">List all of the languages you are offering within this service</p>
-          <div className="mt-2">
-            <input type="text" name="programming-languages" id="programming-languages" autoComplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="HTML, Javascript, PHP..."/>
+        </div> */}
           </div>
         </div>
 
-        <div className="sm:col-span-3">
-          <label htmlFor="token-rate" className="block text-sm font-medium leading-6 text-gray-900">Token Rate</label>
-          <p className="mt-3 text-sm leading-6 text-gray-600">What is the token rate per hour for this service?</p>
-          <div className="mt-2">
-            <input type="text" name="token-rate" id="token-rate" autoComplete="token-rate" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="50"/>
+        <div className="border-b border-gray-900/10 pb-12">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="programming-languages"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Programming Languages
+              </label>
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                List all of the languages you are offering within this service
+              </p>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="programming-languages"
+                  id="programming-languages"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="HTML, Javascript, PHP..."
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="token-rate"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Token Rate
+              </label>
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                What is the token rate per hour for this service?
+              </p>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="token-rate"
+                  id="token-rate"
+                  autoComplete="token-rate"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="50"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-   </div> 
-  <div className="mt-6 flex items-center justify-end gap-x-6">
-    <button type="button" className="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-    <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
-  </div>
-</form>
-    )
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          className="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  );
 }
