2ff423fc4a769e5bf6d967ac780cfdc201734a87
Faisal <faisalmaya@hotmail.com>
2023-05-03 13:49:56 +0100

    removed comments

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -51,12 +51,6 @@ export default function Nav() {
                     Money
                   </a>
 
-                  {/* <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    example 
-                  </a> */}
                 </div>
               </div>
             </div>
