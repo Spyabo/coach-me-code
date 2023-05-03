06ffb416cc2254c886dd7a34d689254a606dfe22
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-03 18:00:17 +0100

    basic styles

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useAuth, useUser } from "@clerk/nextjs";
 
-
 export default function Welcome() {
     const { isLoaded, isSignedIn, user } = useUser();
 
@@ -10,11 +9,28 @@ export default function Welcome() {
         return null;
     }
     return (
-        <div>
-            <h1>Welcome, {user.firstName}</h1>
-            <p>You took the first step! Whether you would like to sharpen the tools in your coding belt through mentoring others, or find a mentor to support you, this is the place! </p>
-            <button><a href="/listings"> Explore Services</a></button>
-            <button><a href="/listings/create-listing">Create a listing</a> </button>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <header>
+                <h1 className="p-4 text-lg font-bold ">Welcome, {user.firstName}!</h1>
+            </header>
+            <main className="container p-4">
+                <p>You took the first step! 🎉 </p>
+                <p> Whether you would like to sharpen the tools in your coding belt through mentoring others, or find a mentor to support you, this is the place for you...</p>
+                <div className="flex gap-5 mt-3">
+                    <div className="container bg-blue-100 p-5 flex flex-col">
+                        <img src="" alt="an overview of our listings" />
+                        <button className="bg-orange-500 text-white text-lg font-bold py-3 px-6 rounded-full mt-6 hover:bg-orange-600 transition-colors duration-300 ease-in-out">
+                            <a href="/listings"> Explore Services</a>
+                        </button>
+                    </div>
+                    <div className="container bg-blue-100 flex justify-center p-5 flex-col">
+                        <img src="" alt="an overview of our listing form" />
+                        <button className="bg-orange-500 text-white text-lg font-bold py-3 px-6 rounded-full mt-6 hover:bg-orange-600 transition-colors duration-300 ease-in-out">
+                            <a href="/listings/create-listing">Create a listing</a>
+                        </button>
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
