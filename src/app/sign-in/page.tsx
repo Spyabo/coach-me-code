f3643c49553d2d9e5d3fe7c03db9d885672826ea
Spyabo <reesewright02@protonmail.com>
2023-05-04 09:35:23 +0100

    Redirect to welcome

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,12 +1,11 @@
 import { SignIn } from "@clerk/nextjs/app-beta";
 
-export default function Page({ searchParams }: any) {
-  const { redirectUrl } = searchParams;
+export default function Page() {
   return (
     <section className="py-24 flex justify-center bg-neutral-50 h-full">
       <div className="container">
         <div className="flex justify-center">
-          <SignIn signUpUrl="/sign-up" redirectUrl={redirectUrl || "/listings"} />
+          <SignIn signUpUrl="/sign-up" redirectUrl={"/welcome"} />
         </div>
       </div>
     </section>
