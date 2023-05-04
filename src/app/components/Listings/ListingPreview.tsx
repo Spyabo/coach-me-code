f61d0b716fbcbda249693371898274e2a7758843
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-04 11:52:58 +0100

    add basic styles

--- src/app/components/Listings/ListingPreview.tsx
+++ src/app/components/Listings/ListingPreview.tsx
@@ -4,29 +4,29 @@ export default async function ListingPreview() {
     const { listings } = await getListings();
 
     return (
-        <div className="container mx-auto">
-            <h1 className="text-xl">Listings</h1>
-            <ul className="flex-auto flex-col gap-9">
-                {listings.map((data: getListingsResponse) => {
-                    return (
-                        <>
-                            <li className="container" key={data._id}>
-                                <h3>{data.listing_title}</h3>
-                                <p>By: {data.name}</p>
-                                <p>Rating: {data.mentor_rating / 10}</p>
-                                <p>
-                                    <img src={data.listing_image} alt={data.listing_title} />
-                                </p>
-                                <h4>Language/s:</h4>
-                                <p>{data.programming_language}</p>
-                                <h4>Description</h4>
-                                <p>{data.listing_description}</p>
-                                <p>Cost: {data.token_rate} tokens</p>
+        <div className="container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <header>
+                <h1 className="text-2xl font-bold my-4">Explore the available services...</h1>
+            </header>
+            <div className="container">
+                <ul className="grid-rows-4 mx-auto">
+                    {listings.map((data: getListingsResponse) => {
+                        return (
+                            <li className="container border-solid border-2 border-indigo-600 p-4 bg-blue-100 flex flex-col justify-center items-center " key={data._id}>
+
+                                <img className="border-solid border-2 border-orange-600" width={300} src={data.listing_image} alt={data.listing_title} />
+                                <h3 className="font-bold text-lg">{data.listing_title}</h3>
+                                <p>{data.name}</p>
+                                <p>{data.mentor_rating / 10}</p>
+                                <p>{data.token_rate} tokens</p>
+                                <button className="bg-orange-500 text-white text-lg font-bold py-3 px-6 rounded-full mt-6 hover:bg-orange-600 transition-colors duration-300 ease-in-out">
+                                    View more
+                                </button>
                             </li>
-                        </>
-                    );
-                })}
-            </ul>
+                        );
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
