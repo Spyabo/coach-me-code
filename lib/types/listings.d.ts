7cf1cb35f765fa54c16e4a03db177f676d1f9582
Spyabo <reesewright02@protonmail.com>
2023-05-04 11:24:17 +0100

    Add postListing type

--- lib/types/listings.d.ts
+++ lib/types/listings.d.ts
@@ -1,20 +1,29 @@
+import { postListing } from "@lib/mongo/listings";
+
 export type listing = {
-    listing_title: string;
-    mentor_rating: number;
-    listing_image: string;
-    listing_description: string;
-    name: string;
-    token_rate: number;
-    programming_languages: string[];
-  };
-  
-  export type getListingsResponse = {
-    _id: string;
-    listing_title: string;
-    mentor_rating: number;
-    listing_image: string;
-    listing_description: string;
-    name: string;
-    token_rate: number;
-    programming_languages: string[];
-  };
+  listing_title: string;
+  mentor_rating: number;
+  listing_image: string;
+  listing_description: string;
+  name: string;
+  token_rate: number;
+  programming_languages: string[];
+};
+
+export type getListingsResponse = {
+  _id: string;
+  listing_title: string;
+  mentor_rating: number;
+  listing_image: string;
+  listing_description: string;
+  name: string;
+  token_rate: number;
+  programming_languages: string[];
+};
+
+export type postListing = {
+  listing_title: string;
+  listing_description: string;
+  programming_languages: string[];
+  token_rate: number;
+};
