d973ad98dfc00bd881e5b8de2ce9d09c684f75a4
Spyabo <reesewright02@protonmail.com>
2023-05-04 11:05:21 +0100

    Move listing types

---
+++ lib/types/listings.d.ts
@@ -0,0 +1,20 @@
+export type listing = {
+    listing_title: string;
+    mentor_rating: number;
+    listing_image: string;
+    listing_description: string;
+    name: string;
+    token_rate: number;
+    programming_languages: string[];
+  };
+  
+  export type getListingsResponse = {
+    _id: string;
+    listing_title: string;
+    mentor_rating: number;
+    listing_image: string;
+    listing_description: string;
+    name: string;
+    token_rate: number;
+    programming_languages: string[];
+  };
