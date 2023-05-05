adb94293b0619383d06109a9f22788259b5b47af
Faisal <faisalmaya@hotmail.com>
2023-05-05 11:13:31 +0100

    updated user types

--- lib/types/users.d.ts
+++ lib/types/users.d.ts
@@ -1,28 +1,30 @@
 export type user = {
-    _id: ObjectId;
-    name: string;
-    email?: string;
-    phone?: string;
-    years_of_experience: number;
-    programming_languages: [];
-    listing_ids: [];
-    order_ids: [];
-    tokens: number;
-  };
-  
-  export type getUsersResponse = {
-    _id: string;
-    name: string;
-    email?: string;
-    phone?: string;
-    years_of_experience: number;
-    programming_languages: [];
-    listing_ids: [];
-    order_ids: [];
-    tokens: number;
-  };
-
-  export type tokenRequest =  {
-    _id: string;
+  _id: ObjectId;
+  clerkAuth: string;
+  name: string;
+  email?: string;
+  phone?: string;
+  years_of_experience: number;
+  programming_languages: [];
+  listing_ids: [];
+  order_ids: [];
   tokens: number;
-  } 
+};
+
+export type getUsersResponse = {
+  _id: string;
+  clerkAuth: string;
+  name: string;
+  email?: string;
+  phone?: string;
+  years_of_experience: number;
+  programming_languages: [];
+  listing_ids: [];
+  order_ids: [];
+  tokens: number;
+};
+
+export type tokenRequest = {
+  _id: string;
+  tokens: number;
+};
