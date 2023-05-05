82a48fa3f3fee3e74f7f4558c6b18107ac3dbd84
Spyabo <reesewright02@protonmail.com>
2023-05-05 09:49:45 +0100

    Fix early return before useState

--- src/app/user-form/page.tsx
+++ src/app/user-form/page.tsx
@@ -4,179 +4,172 @@ import { useState } from 'react';
 
 export default function UserForm() {
   const { isLoaded, isSignedIn, user } = useUser();
- 
-console.log(user)
+
+  const [firstName, setFirstName] = useState(user?.firstName);
+  const [lastName, setLastName] = useState(user?.lastName);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [emailAddress, setEmailAddress] = useState(user?.emailAddresses[0]);
+  const [yearsExperience, setYearsExperience] = useState("");
+  const [programmingLanguages, setProgrammingLanguages] = useState("");
+
   if (!isLoaded || !isSignedIn) {
     return null;
   }
 
-  const [firstName, setFirstName] = useState(user.firstName);
-  const [lastName, setLastName] = useState(user.lastName);
-  const [phoneNumber, setPhoneNumber] = useState(); 
-  const [emailAddress, setEmailAddress] = useState(user.emailAddresses[0]); 
-  const [yearsExperience, setYearsExperience] = useState(); 
-  const [programmingLanguages, setProgrammingLanguages] = useState(""); 
-
+  console.log(user)
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     const newUser = {
-        firstName, 
-        lastName, 
-        phoneNumber,
-        emailAddress,
-        yearsExperience,
-        programmingLanguages, 
-        tokens: 100,
-        reviews: []
+      firstName,
+      lastName,
+      phoneNumber,
+      emailAddress,
+      yearsExperience,
+      programmingLanguages,
+      tokens: 100,
+      reviews: []
     }
-
-
-  
   };
 
   return (
-   
     <form onSubmit={handleSubmit} className="m-8">
-     
-        <div className="border-b">
-            <div className="header flex flex-col my-8">
+      <div className="border-b">
+        <div className="header flex flex-col my-8">
           <h1 className="text-base font-semibold leading-7 text-gray-900">
-            Nearly there! 
+            Nearly there!
           </h1>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             You just need to complete the below form:
           </p>
-          </div>
-        
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="firstName"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                First Name
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                  <input
-                    type="text"
-                    name="firstName"
-                    id="firstName"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={firstName}
-                  onChange={(event) => setFirstName(event.target.value)}
-                  required
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="lastName"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Last Name
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                  <input
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={lastName}
-                  onChange={(event) => setLastName(event.target.value)}
-                  required
-                  />
-                </div>
-              </div>
-            </div>
-                  <div className="sm:col-span-4">
-              <label
-                htmlFor="lastName"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                  <input
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={emailAddress}
-                  onChange={(event) => setEmailAddress(event.target.value)}
-                  required
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="phoneNumber"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Phone number
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                  <input
-                    type="text"
-                    name="phoneNumber"
-                    id="phoneNumber"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={phoneNumber}
-                  onChange={(event) => setPhoneNumber(event.target.value)}
-                  required
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="yearsExperience"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Years of experience
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                  <input
-                    type="text"
-                    name="yearsExperience"
-                    id="yearsExperience"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={yearsExperience}
-                  onChange={(event) => setYearsExperience(event.target.value)}
-                  required
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label
-                htmlFor="programmingLanguages"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Programming languages
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
-                  <input
-                    type="text"
-                    name="programmingLanguages"
-                    id="programmingLanguages"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="HTML, Javascript, PHP..." value={programmingLanguages}
-                  onChange={(event) => setProgrammingLanguages(event.target.value)}
-                  required
-                  />
-                </div>
-              </div>
-            </div>
-      
         </div>
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="firstName"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            First Name
+          </label>
+          <div className="mt-2">
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+              <input
+                type="text"
+                name="firstName"
+                id="firstName"
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="lastName"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Last Name
+          </label>
+          <div className="mt-2">
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="lastName"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+              <input
+                type="text"
+                name="lastName"
+                id="lastName"
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={emailAddress}
+                onChange={(event) => setEmailAddress(event.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="phoneNumber"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Phone number
+          </label>
+          <div className="mt-2">
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+              <input
+                type="text"
+                name="phoneNumber"
+                id="phoneNumber"
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={phoneNumber}
+                onChange={(event) => setPhoneNumber(event.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="yearsExperience"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Years of experience
+          </label>
+          <div className="mt-2">
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+              <input
+                type="text"
+                name="yearsExperience"
+                id="yearsExperience"
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={yearsExperience}
+                onChange={(event) => setYearsExperience(event.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="programmingLanguages"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Programming languages
+          </label>
+          <div className="mt-2">
+            <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+              <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+              <input
+                type="text"
+                name="programmingLanguages"
+                id="programmingLanguages"
+                className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                placeholder="HTML, Javascript, PHP..." value={programmingLanguages}
+                onChange={(event) => setProgrammingLanguages(event.target.value)}
+                required
+              />
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="mt-6 flex items-center justify-center gap-x-6">
         <button
           type="button"
