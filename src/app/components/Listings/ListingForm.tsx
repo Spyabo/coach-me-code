6f17c40a629e5520c8945e140cbad61255fe7d9f
Spyabo <reesewright02@protonmail.com>
2023-05-05 15:22:17 +0100

    set token input autocomplete off

--- src/app/components/Listings/ListingForm.tsx
+++ src/app/components/Listings/ListingForm.tsx
@@ -172,7 +172,7 @@ export default function ListingForm() {
                   type="number"
                   name="token-rate"
                   id="token-rate"
-                  autoComplete="token-rate"
+                  autoComplete="off"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="50"
                   value={listingTokens}
