204315496b66ef49b09b0dac82bd1f27502cbd1e
staceyanne <staceywhite10@gmail.com>
2023-05-05 15:49:06 +0100

    removed unused imports

--- src/app/components/Calendar/ReactCalendar.tsx
+++ src/app/components/Calendar/ReactCalendar.tsx
@@ -1,26 +1,22 @@
-"use client"
+"use client";
 import { format } from "date-fns";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 
 export default function Calendar() {
+  const [selected, setSelected] = useState<Date>();
 
-    const [selected, setSelected] = useState<Date>();
+  let footer = <p>Please pick a day.</p>;
+  if (selected) {
+    footer = <p>You picked {format(selected, "PP")}.</p>;
+  }
 
-    let footer = <p>Please pick a day.</p>;
-    if (selected) {
-        footer = <p>You picked {format(selected, 'PP')}.</p>;
-    }
-
-    console.log(selected, "<==selected")
-    return (
-        <DayPicker
-            mode="single"
-            selected={selected}
-            onSelect={setSelected}
-            footer={footer}
-        />
-    );
+  return (
+    <DayPicker
+      mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      footer={footer}
+    />
+  );
 }
-
-
--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,6 +1,4 @@
-import DateTime from "../components/Calendar/DateTime";
 import Calendar from "../components/Calendar/ReactCalendar";
-import ShadCalendar from "../components/Calendar/ShadCalendar";
 
 export default function Page() {
   return (
@@ -9,21 +7,14 @@ export default function Page() {
         <div className="text-black ">
           <Calendar />
         </div>
-        {/* <div className="px-6 py-6 text-gray-700 text-base">
-          <p className="text-black">Pick a time</p>
-          <DateTime />
-        </div> */}
       </div>
-
 
       <div className="m-10 max-w-md rounded overflow-hidden shadow-lg flex flex-row bg-white ">
         <div className="px-6 py-6 text-gray-700 text-base">
           <h1 className="font-bold text-xl mb-2">Order Summary</h1>
           <p>Listing title</p>
           <img src="" alt="" />
-          <p>
-            Listing description. Lorem ipsum dolor sit amet, consectetur
-          </p>
+          <p>Listing description. Lorem ipsum dolor sit amet, consectetur</p>
           <div className="flex flex-row justify-between py-2">
             <p className="font-bold">Token Balance</p>
             <p>50</p>
@@ -33,10 +24,12 @@ export default function Page() {
             <p>50</p>
           </div>
           <div className="flex flex-row justify-center">
-            <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded-full">Order now</button>
+            <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded-full">
+              Order now
+            </button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
