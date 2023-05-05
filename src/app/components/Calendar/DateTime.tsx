3cd22f940c7434ec8f1e4cb93abdb011a450df55
Freeman-YFH <firegeo@gmail.com>
2023-05-05 13:57:53 +0100

    install shadcn-ui

--- src/app/components/Calendar/DateTime.tsx
+++ src/app/components/Calendar/DateTime.tsx
@@ -1,27 +1,32 @@
-export default function DateTime() {
-    let today = new Date();
-    let dd = today.getDate(); //Current day
-    let mm = today.getMonth() + 1; //January is 0!
-    let yyyy = today.getFullYear(); //(Year is 2022)
-    let hh = today.getHours(); //Current hour
-    let m = today.getMinutes(); //Current minutes
+import { useState } from "react";
 
-    if (dd < 10) {
-        dd = '0' + dd;
-    }
-    if (mm < 10) {
-        mm = '0' + mm;
-    }
-    today = yyyy + '-' + mm + '-' + dd + "T" + hh + ":" + m; //or YYYY-MM-DDThh:mm
+export default function DateTime() {
+    // let today = new Date();
+    // // let dd = today.getDate(); //Current day
+    // // let mm = today.getMonth() + 1; //January is 0!
+    // // let yyyy = today.getFullYear(); //(Year is 2022)
+    // let hh = today.getHours(); //Current hour
+    // let m = today.getMinutes(); //Current minutes
+
+    // if (dd < 10) {
+    //     dd = '0' + dd;
+    // }
+    // if (mm < 10) {
+    //     mm = '0' + mm;
+    // }
+    // today = hh + ":" + m; //or YYYY-MM-DDThh:mm
     // document.getElementById("datefield").setAttribute("min", "max", today);
+
+    // useState
 
     return (
         <>
-            <label htmlFor="meeting-time">Choose a time for your appointment:</label>
+            {/* <label htmlFor="appt">Choose a time for your meeting:</label> */}
 
-            <input type=
-                "datetime-local" name="datefield" id="datefield"
-                min={today} max="todayT11:00" />
+            <input type="time" id="appt" name="appt"
+                min="09:00" max="18:00" required />
+
+            {/* <small>Office hours are 9am to 6pm</small> */}
 
         </>
     )
--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,8 +1,20 @@
 import DateTime from "../components/Calendar/DateTime";
+import Calendar from "../components/Calendar/Calendar";
 
 export default function Page() {
   return (
-    <div className="m-10 flex justify-center">
+    <div className="m-10 flex justify-center flex-row">
+      <div className="flex  bg-white rounded max-w-md p-0">
+        <div className="text-black">
+          <Calendar />
+        </div>
+        <div className="px-6 py-6 text-gray-700 text-base">
+          <p className="text-black">Pick a time</p>
+          <DateTime />
+        </div>
+      </div>
+
+
       <div className="m-10 max-w-md rounded overflow-hidden shadow-lg flex flex-row bg-white ">
         <div className="px-6 py-6 text-gray-700 text-base">
           <h1 className="font-bold text-xl mb-2">Order Summary</h1>
@@ -22,7 +34,6 @@ export default function Page() {
           <div className="flex flex-row justify-center">
             <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded-full">Order now</button>
           </div>
-          <DateTime />
         </div>
       </div>
     </div>
