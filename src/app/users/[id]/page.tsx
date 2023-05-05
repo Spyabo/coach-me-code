a088d08b4af781a3419f4613821ae6d391b5ab9d
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-05 12:02:21 +0100

    add params key to render dynamic route

--- src/app/users/[id]/page.tsx
+++ src/app/users/[id]/page.tsx
@@ -1,3 +1,8 @@
-export default function UserById() {
+import { getUsersById } from "@lib/mongo/users"
+import { user } from "@lib/types/users"
 
+export default async function UserById({ params }: {
+    params: { id: string }
+}) {
+    const user: user = await getUsersById(params.id);
 }
