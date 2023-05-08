91f027342a9786d00cb224f3c2048735c633c44c
Spyabo <reesewright02@protonmail.com>
2023-05-08 22:31:36 +0100

    Add shadcn calendar

--- src/app/components/Calendar/ReactCalendar.tsx
+++ src/app/components/Calendar/ReactCalendar.tsx
@@ -2,9 +2,11 @@
 import { format } from "date-fns";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
+import { Calendar } from "./calendar";
 
 export default function ReactCalendar() {
   const [selected, setSelected] = useState<Date>();
+  const [date, setDate] = useState<Date | undefined>(new Date())
 
   let footer = <p>Please pick a day.</p>;
   if (selected) {
@@ -12,11 +14,19 @@ export default function ReactCalendar() {
   }
 
   return (
-    <DayPicker
-      mode="single"
-      selected={selected}
-      onSelect={setSelected}
-      footer={footer}
-    />
+    <>
+      <DayPicker
+        mode="single"
+        selected={selected}
+        onSelect={setSelected}
+        footer={footer}
+      />
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      />
+    </>
   );
 }
