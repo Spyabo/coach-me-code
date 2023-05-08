afdb1a507372e6b45365757b6259c48c553bc6e2
Spyabo <reesewright02@protonmail.com>
2023-05-08 22:33:03 +0100

    Update imports

--- src/app/components/Calendar/button.tsx
+++ src/app/components/Calendar/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { VariantProps, cva } from "class-variance-authority"
+import * as React from "react"
 
-import { cn } from "@/lib/utils"
+import { cn } from "@lib/utils/shadcn"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
@@ -34,7 +34,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
