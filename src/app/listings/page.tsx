7dc11638e946d9de5388da088924b9721b56fafe
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-09 16:36:17 +0100

    update listing card styles

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -9,20 +9,20 @@ export default async function ListingsPage() {
   return (
     <div className="container max-w-7xl mx-auto sm:px-6 lg:px- mt-20">
       <header>
-        <h1 className="text-2xl font-bold my-4">Explore the available services...</h1>
+        <h1 className="text-2xl font-bold my-6">Explore the available services...</h1>
       </header>
       {/* <ListingFilterBy listingArray={listings} sort_by={"programming_languages"} /> */}
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-2">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-2 ">
         {listings.map((data: getListingsResponse) => {
           return (
-            <li className="container shadow-md rounded border-solid border-2 border-indigo-600 p-4 bg-blue-100 flex flex-col  md:flex-1 justify-between items-center" key={data._id}>
+            <li className="container bg-gray-200 shadow-md rounded border-solid border-2 border-purple-600 flex flex-col gap-4 py-4 md:flex-1 justify-between items-center" key={data._id}>
               <img className="rounded border-solid border-2 border-orange-600" width={300} src={data.listing_image} alt={data.listing_title} />
               <h3 className="font-bold text-lg flex flex-wrap items-center text-center">{data.listing_title}</h3>
               <p>{data.mentor_name}</p>
               <p>{data.listing_rating / 10}</p>
               <p>{data.token_rate} tokens</p>
               <Link href={`/listings/${data._id}`}>
-                <button className="bg-green-400 hover:bg-red-400 text-white text-lg text-center font-bold py-3 px-6 rounded-full mt-6 hover:bg-orange-600 transition-colors duration-300 ease-in-out">
+                <button className="bg-green-400 hover:bg-red-400 text-white text-lg text-center font-bold py-3 px-6 rounded-full hover:bg-orange-600 transition-colors duration-300 ease-in-out">
                   View more
                 </button>
               </Link>
