88e5f41164a3b576fd7151c3a2fb2951483312e2
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-09 15:06:00 +0100

    add red to colour pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,6 +33,17 @@ module.exports = {
         700: "#402a59",
         800: "#2B1347",
     },
+    red: {
+      100: "#fff5f5",
+      200: "#fed7d7",
+      300: "#feb2b2",
+      400: "#fc8181",
+      500: "#f56565",
+      600: "#e53e3e",
+      700: "#c53030",
+      800: "#9b2c2c",
+      900: "#742a2a",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
