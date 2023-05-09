b6c0415901e17b819068ff5df867d8dc7dc29940
Spyabo <reesewright02@protonmail.com>
2023-05-09 15:38:07 +0100

    Change phone input field to type tel

--- src/app/user-form/page.tsx
+++ src/app/user-form/page.tsx
@@ -140,7 +140,7 @@ export default function UserForm() {
             <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
               <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
               <input
-                type="text"
+                type="tel"
                 name="phoneNumber"
                 id="phoneNumber"
                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={phoneNumber}
