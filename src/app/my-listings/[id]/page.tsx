73aa70be25f671343a1c931d353a3f9f0cb157d3
Freeman-YFH <firegeo@gmail.com>
2023-05-09 16:14:10 +0100

    add my-listing page

--- src/app/my-listings/[id]/page.tsx
+++ src/app/my-listings/[id]/page.tsx
@@ -1,13 +1,37 @@
 import { getUserByClerkId } from "@lib/mongo/users";
 import { getUsersResponse } from "@lib/types/users";
-import { user } from "@lib/types/users"
 
 
 export default async function MyListings({ params }) {
-    console.log(params.id, "parms in [id]")
     const user: getUsersResponse = await getUserByClerkId(params.id)
-    console.log(user, "userrr")
     return (
-        <h1>lsitung in slug</h1>
+        <>
+            <div className="flex justify-center text-xl font-medium space-y-10 ">
+
+                <ul>
+                    Listing details:
+                    <li>
+                        Name: {user.name}
+                    </li>
+                    <li>
+                        E-mail: {user.email}
+                    </li>
+                    <li>
+                        Phone number: {user.phone}
+                    </li>
+                    <li>
+                        Years of experience: {user.years_of_experience}
+                    </li>
+                    <li>
+                        Programming languages: {user.programming_languages}
+                    </li>
+                    <li>
+                        Tokens: {user.tokens}
+                    </li>
+                </ul>
+            </div>
+
+        </>
+
     )
 }
