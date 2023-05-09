c780e2f8f045f12954dd71427cfe6fc3f5bdd8bc
Spyabo <reesewright02@protonmail.com>
2023-05-09 09:10:20 +0100

    Remove old calendar

--- src/app/components/Calendar/ReactCalendar.tsx
+++ src/app/components/Calendar/ReactCalendar.tsx
@@ -1,37 +1,24 @@
 "use client";
 import { format } from "date-fns";
 import { useState } from "react";
-import { DayPicker } from "react-day-picker";
 import { Calendar } from "./calendar";
 
 export default function ReactCalendar() {
-  const [selected, setSelected] = useState<Date>();
   const [date, setDate] = useState<Date | undefined>(new Date())
 
-  let footer = <p>Please pick a day.</p>;
-  if (selected) {
-    footer = <p>You picked {format(selected, "PP")}.</p>;
-  }
-  let footer2 = <p>Please pick a day.</p>;
+  let dateSelected = <p>Please pick a day.</p>;
+
   if (date) {
-    footer2 = <p>You picked {format(date!, "PP")}.</p>;
+    dateSelected = <p>You picked {format(date!, "PP")}.</p>;
   }
 
   return (
-    <>
-      <DayPicker
-        mode="single"
-        selected={selected}
-        onSelect={setSelected}
-        footer={footer}
-      />
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border"
-        footer={footer2}
-      />
-    </>
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border"
+      footer={dateSelected}
+    />
   );
 }
