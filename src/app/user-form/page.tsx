b9e4fadc1d564ee8638a16f2935a2c40753511d9
Spyabo <reesewright02@protonmail.com>
2023-05-09 12:29:46 +0100

    Make phone number optional

--- src/app/user-form/page.tsx
+++ src/app/user-form/page.tsx
@@ -145,7 +145,6 @@ export default function UserForm() {
                 id="phoneNumber"
                 className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" value={phoneNumber}
                 onChange={(event) => setPhoneNumber(event.target.value)}
-                required
               />
             </div>
           </div>
