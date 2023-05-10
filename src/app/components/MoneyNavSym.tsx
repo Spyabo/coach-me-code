b845061a04c795079203c3d6dacd27d181e565f4
Faisal <faisalmaya@hotmail.com>
2023-05-10 10:52:07 +0100

    updated money sym

--- src/app/components/MoneyNavSym.tsx
+++ src/app/components/MoneyNavSym.tsx
@@ -1,4 +1,6 @@
-import { getUserByClerkId } from "@lib/mongo/users";
+'use client'
+
+import { useEffect, useState } from "react";
 
 type clerkID = {
     id: string | undefined
@@ -7,15 +9,32 @@ type clerkID = {
 export default async function MoneyNavSym(props: clerkID): Promise<JSX.Element> {
 
     const { id } = props
-    console.log(id)
+    const [tokens, setTokens] = useState<number | null>(null);
 
-    const res = await getUserByClerkId(id)
 
+    // const res = await fetch(`http://localhost:3000/api/users/${id}`);
+    // const result = await res.json()
+    // console.log(result)
+
+    useEffect(() => {
+        async function fetchTokens() {
+            try {
+                const res = await fetch(`http://localhost:3000/api/users/${id}`);
+                const result = await res.json();
+                if (result && result.user && result.user.tokens) {
+                    setTokens(result.user.tokens);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchTokens();
+    }, [id]);
 
 
     return (
         <div>
-            {res.tokens}
+            <p>{tokens}</p>
         </div>
     )
 }
