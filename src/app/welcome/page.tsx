80a98e91d4dc5288b94d381b9d4263c9307d11ca
SamirabintABS <68996388+SamirabintABS@users.noreply.github.com>
2023-05-10 12:15:47 +0100

    delete img folder, update welcome images and page layout

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -10,18 +10,18 @@ export default function Welcome() {
         return null;
     }
     return (
-        <div className="max-w-7xl mx-auto sm:px-6 lg:px- mt-20">
+        <div className="max-w-7xl mx-auto px-6 sm:px-6 lg:px- mt-20">
             <header>
                 <h1 className="text-lg font-bold my-4 ">Welcome, {user.firstName}!</h1>
             </header>
             <main className="container">
                 <p >You took the first step! 🎉 </p>
-                <p> Sharpen the tools in your coding belt through mentoring others, or find a mentor to support you on your coding journey...</p>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-8 my-8">
-                    <div className="container bg-blue-100 p-5 grid justify-items-center shadow-md rounded">
+                <p> Sharpen the tools in your coding belt through coaching others, or find a course to help you on your coding journey...</p>
+                <div className="flex flex-col md:flex-row lg:flex-row gap-8 my-8">
+                    <div className="container p-5 flex flex-col items-center justify-center shadow-md rounded">
                         <h3 className="font-bold text-lg">Want to learn?</h3>
                         <Image
-                            src="/img/1.png"
+                            src="/learn.svg"
                             alt="a graphic of a person studying"
                             width={300}
                             height={300}
@@ -33,7 +33,7 @@ export default function Welcome() {
                     <div className="container bg-blue-100 p-5 grid justify-items-center shadow-xl rounded">
                         <h3 className="font-bold text-lg">Want to coach?</h3>
                         <Image
-                            src="/img/2.png"
+                            src="/teach.svg"
                             alt="A coding coach mentoring a student next to a whiteboard"
                             width={300}
                             height={300}
