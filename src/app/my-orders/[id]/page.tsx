1a2a2c275a721f8b77662006fe013441ec58776b
staceyanne <staceywhite10@gmail.com>
2023-05-10 10:23:13 +0100

    added function to return list of orders

--- src/app/my-orders/[id]/page.tsx
+++ src/app/my-orders/[id]/page.tsx
@@ -1,11 +1,27 @@
 import { getUserByClerkId } from "@lib/mongo/users";
 import { getListings } from "@lib/mongo/listings";
+import { getUsersResponse } from "@lib/types/users";
+
 
 
 export default async function MyOrders({ params }) {
-    // const { listings }: getListingsResponse = await getUserByClerkId(params.id)
+    const user: getUsersResponse = await getUserByClerkId(params.id);
+    const { listings } = await getListings()
+    const orders = user.order_ids;
+
     return (
 
-       
+        <h1 className="bg-black">Hello</h1>
     )
+
+    const listing = orders.map((order) => {
+        let result = listings.find(listing => listing._id === order)
+        console.log(result)
+        return result;
+    })
+
+
+
+
+
 }
