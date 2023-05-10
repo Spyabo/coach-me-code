94874c5050ff54a63a903c1f99310127fd6c6a8d
Spyabo <reesewright02@protonmail.com>
2023-05-10 16:13:17 +0100

    Add uploadathing route

---
+++ src/app/api/uploadathing/route.ts
@@ -0,0 +1,8 @@
+/** app/api/uploadthing/route.ts */
+import { createNextRouteHandler } from "uploadthing/server";
+import { ourFileRouter } from "./core";
+
+// Export routes for Next App Router (/pages/api support coming soon!)
+export const { POST } = createNextRouteHandler({
+  router: ourFileRouter,
+});
