1ad14fff18111add9161c51b9abba680976b2cd0
Spyabo <reesewright02@protonmail.com>
2023-05-10 21:09:08 +0100

    Add clerk user

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,18 +1,17 @@
+import { useUser } from "@clerk/nextjs";
 import { createFilething, type FileRouter } from "uploadthing/server";
 const f = createFilething();
-
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f
     // Set permissions and file types for this FileRoute
-    .fileTypes(["image", "video"])
-    .maxSize("1GB")
+    .fileTypes(["image"])
+    .maxSize("1MB")
     .middleware(async (req) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+      const { user } = useUser();
 
       // If you throw, the user will not be able to upload
       if (!user) throw new Error("Unauthorized");
