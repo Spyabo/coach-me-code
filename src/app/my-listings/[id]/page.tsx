4fb226f45bd5e861ffb8c65d4fa83b04bd2518d2
Freeman-YFH <firegeo@gmail.com>
2023-05-10 08:50:59 +0100

    add listing page by id

--- src/app/my-listings/[id]/page.tsx
+++ src/app/my-listings/[id]/page.tsx
@@ -1,37 +1,62 @@
+import { getListingByClerkId } from "@lib/mongo/listings";
 import { getUserByClerkId } from "@lib/mongo/users";
 import { getUsersResponse } from "@lib/types/users";
+import { getListingsResponse } from "@lib/types/listings";
+
 
 
 export default async function MyListings({ params }) {
-    const user: getUsersResponse = await getUserByClerkId(params.id)
+    // const user: getUsersResponse = await getUserByClerkId(params.id)
+    const { listings }: getListingsResponse = await getListingByClerkId(params.id)
     return (
-        <>
-            <div className="flex justify-center text-xl font-medium space-y-10 ">
 
+        <>
+            <div>
                 <ul>
-                    Listing details:
-                    <li>
-                        Name: {user.name}
-                    </li>
-                    <li>
-                        E-mail: {user.email}
-                    </li>
-                    <li>
-                        Phone number: {user.phone}
-                    </li>
-                    <li>
-                        Years of experience: {user.years_of_experience}
-                    </li>
-                    <li>
-                        Programming languages: {user.programming_languages}
-                    </li>
-                    <li>
-                        Tokens: {user.tokens}
-                    </li>
+
+                    {listings.map((listing) => {
+                        return (
+                            <li key={listing._id}>
+                                <p>Listing title: {listing.listing_title}</p>
+                                <p>Listing description: {listing.listing_description}</p>
+                                <p>Programming languages: {(listing.programming_languages).map(item => item)}</p>
+                                <p>Token rate: {listing.token_rate}</p>
+                            </li>
+                        )
+                    })}
+
                 </ul>
             </div>
-
         </>
+
+
+
+        // <>
+        //     <div className="flex justify-center text-2xl font-medium  ">
+        //         <ul>
+        //             Listing details:
+        //             <li>
+        //                 Name: {user.name}
+        //             </li>
+        //             <li>
+        //                 E-mail: {user.email}
+        //             </li>
+        //             <li>
+        //                 Phone number: {user.phone}
+        //             </li>
+        //             <li>
+        //                 Years of experience: {user.years_of_experience}
+        //             </li>
+        //             <li>
+        //                 Programming languages: {user.programming_languages}
+        //             </li>
+        //             <li>
+        //                 Tokens: {user.tokens}
+        //             </li>
+        //         </ul>
+        //     </div>
+
+        // </>
 
     )
 }
