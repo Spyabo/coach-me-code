91fce9d4d03a15f040d140ed53c04fb11fa23e21
staceyanne <staceywhite10@gmail.com>
2023-05-10 09:52:28 +0100

    added function

---
+++ src/app/my-orders/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { getUserByClerkId } from "@lib/mongo/users";
+import { getListings } from "@lib/mongo/listings";
+
+
+export default async function MyOrders({ params }) {
+    // const { listings }: getListingsResponse = await getUserByClerkId(params.id)
+    return (
+
+       
+    )
+}
