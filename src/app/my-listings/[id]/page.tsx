3dd2aeb72c55c29d34d2f2278ce3a40632a70206
Freeman-YFH <firegeo@gmail.com>
2023-05-10 09:51:03 +0100

    modify my-lising css

--- src/app/my-listings/[id]/page.tsx
+++ src/app/my-listings/[id]/page.tsx
@@ -5,14 +5,14 @@ export default async function MyListings({ params }) {
     const { listings }: getListingsResponse = await getListingByClerkId(params.id)
     return (
 
-        listings.length === 0 ? <h1 className="flex justify-center text-4xl">No listing yet!</h1> :
+        listings.length === 0 ? <h1 className="container max-w-7xl mx-auto sm:px-6 lg:px- mt-20">No listing yet!</h1> :
 
             <>
-                <div className="flex flex-row justify-center">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-2">
                     <ul>
                         {listings.map((listing) => {
                             return (
-                                <li key={listing._id}>
+                                <li className="container bg-gray-200 shadow-md rounded border-solid border-2 border-purple-600 flex flex-col gap-4 py-4 md:flex-1 justify-between items-center" key={listing._id}>
                                     <p>Listing title: {listing.listing_title}</p>
                                     <p>Listing description: {listing.listing_description}</p>
                                     <p>Programming languages: {(listing.programming_languages).map(item => item)}</p>
