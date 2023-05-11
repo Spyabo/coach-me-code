c120e54aa124c3ccaa27cdfb4883090e3a2e39be
Spyabo <reesewright02@protonmail.com>
2023-05-11 10:04:37 +0100

    Add conditional render on order

--- src/app/listings/[id]/purchase/page.tsx
+++ src/app/listings/[id]/purchase/page.tsx
@@ -1,4 +1,6 @@
+//@ts-nocheck
 "use client"
+import Confirmation from "@/app/components/Confirmation";
 import { useUser } from "@clerk/nextjs";
 import format from "date-fns/format";
 import { usePathname } from "next/navigation";
@@ -7,6 +9,7 @@ import ReactCalendar from "../../../components/Calendar/ReactCalendar";
 
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date())
+  const [ordered, setOrdered] = useState(false);
   const listing_id = usePathname().split("/")[2];
   const { user } = useUser();
   const order = { [listing_id]: date?.toISOString() };
@@ -21,14 +24,23 @@ export default function Page() {
         bool: true
       })
     })
+    if (date) setOrdered(true);
   }
 
-  return (
+  return (ordered ?
+    <div className="flex justify-center text-center md:flex-row m-10 p-12 gap-6">
+      <div className="flex flex-col justify-around max-w-md rounded shadow-lg bg-white p-4  border-solid border-2 border-purple-200">
+        <h1 className="font-bold">Congratulations! You have booked a mentor!</h1>
+        <div className="py-4">
+          <p>Your booking is on {date?.toString()}</p>
+          <p></p>
+        </div>
+      </div>
+    </div> :
     <div className="flex justify-center md:flex-row m-10 py-6 gap-6 ">
       <div className=" bg-white rounded max-w-md p-6 shadow-lg">
         <ReactCalendar date={date} setDate={setDate} />
       </div>
-
       <div className="flex flex-col justify-around items-center max-w-md rounded shadow-lg bg-white ">
         <div className="px-6 py-6 text-gray-700 text-base">
           <h1 className="font-bold text-xl mb-2">Order Summary</h1>
