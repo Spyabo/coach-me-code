44c88d6be1a79cfabc8c62771b35b3259cc006aa
Spyabo <reesewright02@protonmail.com>
2023-05-11 15:35:10 +0100

    Fix programming languages autocomplete

--- src/app/components/Listings/ListingForm.tsx
+++ src/app/components/Listings/ListingForm.tsx
@@ -166,7 +166,7 @@ export default function ListingForm() {
                   type="text"
                   name="programming-languages"
                   id="programming-languages"
-                  autoComplete="programming-languages"
+                  autoComplete="off"
                   className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   placeholder="TypeScript, Python, Rust..."
                   value={programmingLanguages}
