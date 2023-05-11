d0c8d1dcda7c91ee217706bbbcba20a63548f526
Spyabo <reesewright02@protonmail.com>
2023-05-11 09:32:56 +0100

    Change programming languages placeholder

--- src/app/components/Listings/ListingForm.tsx
+++ src/app/components/Listings/ListingForm.tsx
@@ -182,7 +182,7 @@ export default function ListingForm() {
                   id="programming-languages"
                   autoComplete="programming-languages"
                   className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  placeholder="HTML, Javascript, PHP..."
+                  placeholder="TypeScript, Python, Rust..."
                   value={programmingLanguages}
                   onChange={(event) =>
                     setProgrammingLanguages(event.target.value)
