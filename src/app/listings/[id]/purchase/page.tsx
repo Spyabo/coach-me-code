a6f0a47d5e1d1fc9b9909356f3f6e2ce329d999e
Spyabo <reesewright02@protonmail.com>
2023-05-11 10:41:34 +0100

    Fetch data on current listing

--- src/app/listings/[id]/purchase/page.tsx
+++ src/app/listings/[id]/purchase/page.tsx
@@ -1,10 +1,10 @@
 //@ts-nocheck
 "use client"
-import Confirmation from "@/app/components/Confirmation";
 import { useUser } from "@clerk/nextjs";
 import format from "date-fns/format";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { listing } from '../../../../../lib/types/listings';
 import ReactCalendar from "../../../components/Calendar/ReactCalendar";
 
 export default function Page() {
@@ -13,6 +13,16 @@ export default function Page() {
   const listing_id = usePathname().split("/")[2];
   const { user } = useUser();
   const order = { [listing_id]: date?.toISOString() };
+  const [listingData, setListingData] = useState();
+
+  useEffect(() => {
+    async function fetchListing() {
+      const res = await fetch(`http://localhost:3000/api/${listing_id}/listing`);
+      const { listing } = await res.json();
+      setListingData(listing);
+    }
+    fetchListing();
+  }, [listing_id])
 
   const handleOnClick = async () => {
     const res = await fetch("/api/users", {
@@ -44,8 +54,8 @@ export default function Page() {
       <div className="flex flex-col justify-around items-center max-w-md rounded shadow-lg bg-white ">
         <div className="px-6 py-6 text-gray-700 text-base">
           <h1 className="font-bold text-xl mb-2">Order Summary</h1>
-          <p>Listing title</p>
-          <p>Listing description. Lorem ipsum dolor sit amet, consectetur</p>
+          <p>{listingData?.listing_title}</p>
+          <p>{listingData?.listing_description.split(" ").slice(0, 20).join(" ").concat("...")}</p>
           <div className="flex flex-row justify-between py-2">
             <p className="font-bold">Token Balance</p>
             <p>50</p>
