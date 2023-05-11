c6364759f093b1756e11ebbd58d9cc5d464dfa84
Spyabo <reesewright02@protonmail.com>
2023-05-11 11:32:38 +0100

    Styling

--- src/app/users/[id]/my-orders/page.tsx
+++ src/app/users/[id]/my-orders/page.tsx
@@ -11,7 +11,7 @@ export default async function Page({ params }) {
     return (
         orders.length === 0 ? <h1 className="flex justify-center text-4xl p-10 font-bold">No orders yet!</h1> :
             <div className="flex flex-col justify-center">
-                <h1 className="text-2xl font-bold mt-16 text-center p-4">
+                <h1 className="text-3xl font-bold mt-16 text-center p-4 underline">
                     Your Order History
                 </h1>
                 <div>
@@ -26,26 +26,27 @@ export default async function Page({ params }) {
                         return (
                             <div
                                 key={listingId}
-                                className="bg-white rounded-lg shadow-lg mx-auto m-10 flex flex-col border-solid border-2 border-purple-600 overflow-x-auto whitespace-normal "
+                                className="bg-gray-300 rounded-lg shadow-lg m-10 flex flex-col border-solid border-2 border-purple-600 overflow-x-auto whitespace-normal "
                             >
-                                <div className="bg-gray-300 p-2 rounded">
+                                <div className=" bg-purple-800 text-white p-2 rounded">
                                     <div className="flex flex-row justify-between sm:flex-col">
                                         <p className="font-bold">Order #{listingId}</p>
-                                        <p>Total: {orderItem.token_rate} tokens</p>
+                                        <p>Total Price: {orderItem?.token_rate} tokens</p>
                                     </div>
-                                    <p>Purchased on {date}</p>
+                                    <p>Booking Date: {date}</p>
                                 </div>
-                                <div className="flex align-center flex-col">
-                                    <div className="flex flex-row">
+                                <div className="flex justify-center items-center text-center flex-col">
+                                    <div>
                                         <img
-                                            src={orderItem.listing_image}
-                                            className="rounded p-4"
-                                            width={150}
-                                            alt={orderItem.listing_title}
+                                            src={orderItem?.listing_image}
+                                            className="rounded shadow-md mt-2"
+                                            height={400}
+                                            width={400}
+                                            alt={orderItem?.listing_title}
                                         />
                                         <div className="p-4">
-                                            <p>{orderItem.listing_title}</p>
-                                            <p>{orderItem.mentor_name}</p>
+                                            <p>{orderItem?.listing_title}</p>
+                                            <p>{orderItem?.mentor_name}</p>
 
                                             <a href={`/listings/${order}`}>
                                                 <button className="bg-green-400 hover:bg-red-400 text-white font-bold py-2 px-2 m-2 rounded-full">
