d733b700d9516b06019f33e4f8e17b5a51c984da
Spyabo <reesewright02@protonmail.com>
2023-05-15 19:32:51 +0100

    Fix types + add err guards

--- src/app/users/[id]/my-orders/page.tsx
+++ src/app/users/[id]/my-orders/page.tsx
@@ -4,9 +4,15 @@ import { getListingsResponse } from "@lib/types/listings";
 import { getUsersResponse } from "@lib/types/users";
 import Image from "next/image";
 
-export default async function Page({ params }) {
-  const user: getUsersResponse = await getUserByClerkId(params.id);
+export default async function Page({ params }: { params: { id: string } }) {
+  const user: getUsersResponse | { error: string } = await getUserByClerkId(
+    params.id
+  );
   const { listings } = await getListings();
+
+  if ("error" in user) return <div>{user.error}</div>;
+  if (!listings) return <div>No listings found</div>;
+
   const orders = user.order_ids;
 
   return orders.length === 0 ? (
@@ -29,6 +35,8 @@ export default async function Page({ params }) {
               listing._id === Object.keys(order)[0]
           );
 
+          if (!orderItem) return;
+
           return (
             <div
               key={listingId}
@@ -37,22 +45,22 @@ export default async function Page({ params }) {
               <div className="flex flex-col bg-purple-800 text-white p-2 rounded">
                 <div className="flex flex-col justify-between sm:flex-col">
                   <p className="font-bold">Order: #{listingId}</p>
-                  <p>Total Price: {orderItem?.token_rate} tokens</p>
+                  <p>Total Price: {orderItem.token_rate} tokens</p>
                 </div>
                 <p>Booking Date: {date}</p>
               </div>
               <div className="flex justify-center items-center text-center flex-col">
                 <div>
                   <Image
-                    src={orderItem?.listing_image}
+                    src={orderItem.listing_image}
                     className="rounded shadow-md mt-2"
                     height={400}
                     width={400}
-                    alt={orderItem?.listing_title}
+                    alt={orderItem.listing_title}
                   />
                   <div className="p-4">
-                    <p>{orderItem?.listing_title}</p>
-                    <p>{orderItem?.mentor_name}</p>
+                    <p>{orderItem.listing_title}</p>
+                    <p>{orderItem.mentor_name}</p>
 
                     <a href={`/listings/${order}`}>
                       <button className="bg-red-500 hover:bg-yellow-400 text-white font-bold py-2 px-2 m-2 rounded-full">
